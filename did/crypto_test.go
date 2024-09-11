64936fd061ea042228f1724330374f4f7ce59ab0
Steve Moyer <smoyer1@selesy.com>
2024-09-11 07:12:54 -0400

    feat(did): add ToPubKey() and improve crypto tests

--- did/crypto_test.go
+++ did/crypto_test.go
@@ -1,33 +1,51 @@
 package did_test
 
 import (
-	"bytes"
-	"encoding/binary"
 	"testing"
 
 	"github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/multiformats/go-multicodec"
 	"github.com/stretchr/testify/require"
 	"github.com/ucan-wg/go-ucan/v1/did"
+)
+
+const (
+	exampleDIDStr    = "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"
+	examplePubKeyStr = "Lm/M42cB3HkUiODQsXRcweM6TByfzEHGO9ND274JcOY="
 )
 
 func TestFromPubKey(t *testing.T) {
 	t.Parallel()
 
-	const example = "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"
+	id, err := did.FromPubKey(examplePubKey(t))
+	require.NoError(t, err)
+	require.Equal(t, exampleDID(t), id)
+}
 
-	id, err := did.Parse(example)
+func TestToPubKey(t *testing.T) {
+	t.Parallel()
+
+	pubKey, err := did.ToPubKey(exampleDIDStr)
+	require.NoError(t, err)
+	require.Equal(t, examplePubKey(t), pubKey)
+}
+
+func exampleDID(t *testing.T) did.DID {
+	t.Helper()
+
+	id, err := did.Parse(exampleDIDStr)
 	require.NoError(t, err)
 
-	buf := bytes.NewBuffer(id.Bytes())
+	return id
+}
 
-	code, err := binary.ReadUvarint(buf)
+func examplePubKey(t *testing.T) crypto.PubKey {
+	t.Helper()
+
+	pubKeyCfg, err := crypto.ConfigDecodeKey(examplePubKeyStr)
 	require.NoError(t, err)
-	require.Equal(t, uint64(multicodec.Ed25519Pub), code)
 
-	pubKey, err := crypto.UnmarshalEd25519PublicKey(buf.Bytes())
+	pubKey, err := crypto.UnmarshalEd25519PublicKey(pubKeyCfg)
 	require.NoError(t, err)
 
-	id2, err := did.FromPubKey(pubKey)
-	require.Equal(t, id, id2)
+	return pubKey
 }
