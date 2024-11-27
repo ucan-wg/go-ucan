6f4853cd2f1f4fd13f44f9dbc82f07e9459bbce8
Michael Muré <batolettre@gmail.com>
2024-11-27 12:23:56 +0100

    bearer,context: support hash verification of the computed args

--- toolkit/server/context.go
+++ toolkit/server/context.go
@@ -2,27 +2,26 @@ package server
 
 import "context"
 
-type contextKey string
-
-var ctxUserId = contextKey("userId")
-var ctxProjectId = contextKey("projectId")
+type ctxUserIdKey struct{}
 
 // ContextGetUserId return the UserId stored in the context, if it exists.
 func ContextGetUserId(ctx context.Context) (string, bool) {
-	val, ok := ctx.Value(ctxUserId).(string)
+	val, ok := ctx.Value(ctxUserIdKey{}).(string)
 	return val, ok
 }
 
 func addUserIdToContext(ctx context.Context, userId string) context.Context {
-	return context.WithValue(ctx, ctxUserId, userId)
+	return context.WithValue(ctx, ctxUserIdKey{}, userId)
 }
+
+type ctxProjectIdKey struct{}
 
 // ContextGetProjectId return the ProjectID stored in the context, if it exists.
 func ContextGetProjectId(ctx context.Context) (string, bool) {
-	val, ok := ctx.Value(ctxProjectId).(string)
+	val, ok := ctx.Value(ctxProjectIdKey{}).(string)
 	return val, ok
 }
 
 func addProjectIdToContext(ctx context.Context, projectId string) context.Context {
-	return context.WithValue(ctx, ctxProjectId, projectId)
+	return context.WithValue(ctx, ctxProjectIdKey{}, projectId)
 }
