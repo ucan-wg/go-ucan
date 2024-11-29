bff482f73b98f18bfb7a6fb8c0b9a86af5697f03
Fabio Bozzo <fabio.bozzo@consensys.net>
2024-11-29 13:04:14 +0100

    add constants.go

---
+++ pkg/policy/limits/constants.go
@@ -0,0 +1,8 @@
+package limits
+
+const (
+	// MaxInt53 represents the maximum safe integer in JavaScript (2^53 - 1)
+	MaxInt53 = 9007199254740991
+	// MinInt53 represents the minimum safe integer in JavaScript (-2^53 + 1)
+	MinInt53 = -9007199254740991
+)
